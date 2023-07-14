2097b99d0e7d23983b392af0e021b50f4e22c743
carlo1700 <lombardi.1883998@studenti.uniroma1.it>
2023-07-14 17:36:44 +0200

    non definitivo ma non da più errori

--- progetto_lampioni/main.py
+++ progetto_lampioni/main.py
@@ -1,5 +1,8 @@
 import cv2
+import roboflow
 import numpy as np
+from sklearn.metrics import precision_score, recall_score, f1_score
+
 
 # Funzione per l'individuazione degli illuminanti
 def individua_illuminanti_immagine(image):
@@ -9,15 +12,17 @@ def individua_illuminanti_immagine(image):
 
 
     # Applica una soglia per estrarre i punti di luce
-    _, threshold = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    _, threshold = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
     cv2.imshow('threshold', threshold)
 
     # Applica un filtro per rimuovere il rumore di fondo
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+    #immagine binaria
     opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
     cv2.imshow('opening', opening)
 
-    # Trova le componenti connesse
+    # Trova le componenti connesse nell'immagine binaria
     contours, _ = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     illuminanti = []
@@ -28,29 +33,83 @@ def individua_illuminanti_immagine(image):
         area = cv2.contourArea(contour)
 
         # Filtra i falsi positivi in base all'area
-        if area > 10 and area < 5000:
+        if 10 < area < 500:
             # Calcola il centroide del contorno
             moments = cv2.moments(contour)
-            centroid_x = int(moments['m10'] / moments['m00'])
-            centroid_y = int(moments['m01'] / moments['m00'])
-
             # Aggiungi il centroide agli illuminanti individuati
-            illuminanti.append((centroid_x, centroid_y))
+            illuminanti.append((
+                moments['m10'] // moments['m00'],
+                moments['m01'] // moments['m00']
+            ))
 
     return illuminanti
 
 # Carica l'immagine
-# image = cv2.imread('lab_img.jpg')
-image = cv2.imread('C:\\Users\\Administrator\\Pictures\\lab_ia\\progetto_lampioni\\data\\immagine_lab_ia (1).jpg')
+image = cv2.imread('roboflow\\test\\images\\immagine_lab_ia-1-_jpg.rf.bbe7082e00bc3ec42ff243f08fd0349b.jpg')
 
 # Individua gli illuminanti nell'immagine
 illuminanti = individua_illuminanti_immagine(image)
 
 # Disegna i centroidi degli illuminanti sull'immagine originale
 for illuminante in illuminanti:
-    cv2.circle(image, illuminante, 5, (0, 255, 0), -1)
+    x, y = map(int, illuminante)  # Converti le coordinate in valori interi
+    cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
+
 
 # Visualizza l'immagine con gli illuminanti individuati
 cv2.imshow('Illuminanti individuati', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Carica l'immagine etichettata da Roboflow
+etichette_file = 'roboflow\\test\\labels\\immagine_lab_ia-1-_jpg.rf.bbe7082e00bc3ec42ff243f08fd0349b.txt'
+
+# Leggi le etichette dal file
+
+
+with open(etichette_file, 'r') as file:
+    etichette = [tuple(map(float, line.strip().split())) for line in file]
+
+# Stampa la lunghezza delle etichette e delle predizioni
+
+
+
+# Individua gli illuminanti nell'immagine
+illuminanti = individua_illuminanti_immagine(image)
+
+# ...
+
+# Inizializzazione della soglia di distanza
+soglia_distanza = 10.0  # Imposta una soglia di distanza iniziale arbitraria
+
+# Prepara le predizioni e le etichette corrette per il calcolo delle metriche
+predizioni = np.zeros(len(illuminanti), dtype=bool)
+for i, (x_predetto, y_predetto) in enumerate(illuminanti):
+    # Effettua il confronto con le etichette corrette
+    for etichetta in etichette:
+        if np.sqrt((x_predetto - etichetta[0]) ** 2 + (y_predetto - etichetta[1]) ** 2) <= soglia_distanza:
+            predizioni[i] = True
+            break
+
+print("le etichette sono: ", len(etichette))
+print("le predizioni sono: ", len(predizioni))
+
+# ...
+
+
+# Calcola le metriche di precisione, richiamo e F1-score
+if len(etichette) != len(predizioni):
+    print("Errore: il numero di etichette e predizioni non corrisponde.")
+else:
+    # Calcola la precisione
+    precision = precision_score(etichette, predizioni)
+    print("Precisione:", precision)
+
+    # Calcola la recall
+    recall = recall_score(etichette, predizioni)
+    print("Recall:", recall)
+
+    # Calcola l'F1-score
+    f1 = f1_score(etichette, predizioni)
+    print("F1-score:", f1)
+
---
+++ progetto_lampioni/roboflow/README.dataset.txt
@@ -0,0 +1,6 @@
+# progetto_lampioni > 2023-07-13 6:43pm
+https://universe.roboflow.com/lab-ia-mqwan/progetto_lampioni
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
